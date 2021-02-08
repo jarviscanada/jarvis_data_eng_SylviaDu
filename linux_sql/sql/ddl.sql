3c0336b6bea6954640305d87c31a7f7296aae499
Sylvia Du <sylviadu97@gmail.com>
2021-02-08 22:05:55 +0000

    Modified ddl.sql

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,24 @@
+-- Creates two tables, host_info and host_usage, if they don't already exist
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
+  id SERIAL NOT NULL, 
+  hostname VARCHAR UNIQUE NOT NULL, 
+  cpu_number INT NOT NULL, 
+  cpu_architecture VARCHAR NOT NULL, 
+  cpu_model VARCHAR NOT NULL, 
+  cpu_mhz FLOAT NOT NULL, 
+  L2_cache INT NOT NULL, 
+  total_mem INT NOT NULL, 
+  timestamp TIMESTAMP NOT NULL, 
+  PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
+  "timestamp" TIMESTAMP NOT NULL,
+  host_id SERIAL NOT NULL, 
+  memory_free INT NOT NULL, 
+  cpu_idle INT NOT NULL, 
+  cpu_kernel INT NOT NULL, 
+  disk_io INT NOT NULL, 
+  disk_available INT NOT NULL, 
+  FOREIGN KEY (host_id) REFERENCES host_info (id)
+);
